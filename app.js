3cd86360a57ccac3114950df28126e27865b513d
Perez <rperez2@labs.neit.edu>
2019-03-13 12:08:54 -0400

    Working on Things

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var router = express.Router();
+var port = process.env.PORT || 5000;
+
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+
+router.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname+'/login.html'));
+});
+
+
+router.get('/addGame', function(req, res){
+    res.sendFile(path.join(__dirname+'/addGame.html'));
+});
+
+app.use('/', router);
+app.listen(port, function(){
+    console.log("server is running on port: " + port);
+});
